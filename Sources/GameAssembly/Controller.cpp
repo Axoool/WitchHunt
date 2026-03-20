7d73c19558aa5a99f065c5420264f38be5b3235c
Axel Martin <amartin@gaming.tech>
2026-03-20 17:35:57 +0100

    added serialization to controller script component

--- Sources/GameAssembly/Controller.cpp
+++ Sources/GameAssembly/Controller.cpp
@@ -9,3 +9,17 @@ void Controller::Update(float deltaTime)
 {
 
 }
+
+void Controller::Serialize(nlohmann::json& out) const
+{
+	out["moveSpeed"] = m_moveSpeed;
+	out["sprintSpeed"] = m_sprintSpeed;
+	out["sensitivity"] = m_sensitivity;
+}
+
+void Controller::Deserialize(const nlohmann::json& in)
+{
+	if (in.contains("moveSpeed")) m_moveSpeed = in["moveSpeed"];
+	if (in.contains("sprintSpeed")) m_sprintSpeed = in["sprintSpeed"];
+	if (in.contains("sensitivity")) m_sensitivity = in["sensitivity"];
+}
